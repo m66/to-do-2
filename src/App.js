c3119061ba2668c9d1bfbb878ba46de7e55b700b
user <AshotHeb>
2022-04-28 20:31:52 +0400

    last changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-import { H1 } from "./H1/H1";
-import { P } from "./P/P";
-
-function Image() {
-  return (
-    <div>
-      <img
-        src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?cs=srgb&dl=pexels-lukas-rodriguez-3680219.jpg&fm=jpg"
-        alt="image"
-      />
-    </div>
-  );
-}
-
 function App() {
   return <div className="App"></div>;
 }
